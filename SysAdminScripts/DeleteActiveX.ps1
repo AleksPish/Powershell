fb69055666db8e0bf9d7778be43092d83e6c81cc
Aleks Pish <51082993+AleksPish@users.noreply.github.com>
2021-07-06 09:28:17 +0100

    6/7/21

--- SysAdminScripts/DeleteActiveX.ps1
+++ SysAdminScripts/DeleteActiveX.ps1
@@ -2,6 +2,6 @@ $Count = 0
 $Files = Get-ChildItem $env:userprofile -filter *noActiveX* -Recurse 
 ForEach  ($File in $Files) {
     $Count++
-    Remove-item $File
+    Remove-item $File -ErrorAction Continue
 }
 Write-host $Count" files deleted"
