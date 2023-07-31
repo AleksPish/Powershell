1a901307320d3eee953744cba71e804063c384ee
AleksPish <aleks@piszczynski.com>
2023-07-31 17:03:09 +0100

    draft commit

---
+++ SysAdminScripts/List all installed features.ps1
@@ -0,0 +1,5 @@
+#List all installed features
+Get-WindowsFeature | Where-Object {$_. installstate -eq "installed"} | Format-List Name,Installstate | more
+
+#List Features on remote host
+Get-WindowsFeature -ComputerName dc01 | Where-Object {$_. installstate -eq "installed"} | Format-List Name,Installstate | more
