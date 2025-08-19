358527343470e88c25d05830f34b3ae70a4c0e8c
Aleks Pish <51082993+AleksPish@users.noreply.github.com>
2025-08-19 16:56:43 +0100

    update

--- HandyTasksScripts/OpenSSL-ExtractCert.ps1
+++ HandyTasksScripts/OpenSSL-ExtractCert.ps1
@@ -5,3 +5,8 @@ openssl pkcs12 -in input.pfx -clcerts -nokeys -out cert.pem
 
 #Extract key
 openssl pkcs12 -in input.pfx -nocerts -out encrypted.key
+
+#Get thumbprint from cert
+openssl pkcs12 -in $certfile -nokeys -out cert.pem;
+
+openssl x509 -in cert.pem -fingerprint -noout -sha256
